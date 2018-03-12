604c48b2ab2fd97ead5d69a1d3fd83159624de56
Adrian Gonzalo <gonzahh@gmail.com>
2018-03-13 00:12:23 +0100

    added basic structure of express application

---
+++ app/utils/errorHandlers.js
@@ -0,0 +1,78 @@
+// requires:
+const logger = require('./logger');
+
+
+// functions:
+
+/**
+ * Handles express response when a route does
+ * not exists. Express typical input params:
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.notFound = (req, res, next) => {
+  const reqData = {
+    userAgent: req.headers['user-agent'],
+    url: req.url,
+    method: req.method,
+  };
+  logger.error(`404 error: ${JSON.stringify(reqData)}`);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+};
+
+/**
+ * Handles express response on error when is
+ * development environment. Express typical input
+ * params:
+ * @param {*} err
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.developmentErrors = (err, req, res) => {
+  err.stack = err.stack || '';
+  const errorDetails = {
+    message: err.message,
+    status: err.status,
+    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>'),
+  };
+  const reqData = {
+    userAgent: req.headers['user-agent'],
+    url: req.url,
+    method: req.method,
+    errorMessage: errorDetails.message,
+    errorStatus: errorDetails.status,
+  };
+  logger.error(`express developmentError: ${JSON.stringify(reqData)}`);
+  res.status(err.status || 500);
+  res.send(JSON.stringify(errorDetails));
+};
+
+/**
+ * Handles express response on error when is
+ * production environment. Express typical input
+ * params:
+ * @param {*} err
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.productionErrors = (err, req, res) => {
+  const errorDetails = {
+    message: err.message,
+    status: err.status,
+  };
+  const reqData = {
+    userAgent: req.headers['user-agent'],
+    url: req.url,
+    method: req.method,
+    errorMessage: errorDetails.message,
+    errorStatus: errorDetails.status,
+  };
+  logger.error(`express productionError: ${JSON.stringify(reqData)}`);
+  res.status(err.status || 500);
+  res.send(JSON.stringify(err.message));
+};
--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 // requires:
+const express = require('express');
+const bodyParser = require('body-parser');
+const { notFound, developmentErrors, productionErrors  } = require('./app/utils/errorHandlers');
 const logger = require('./app/utils/logger');
 
+require('dotenv').config();
+
+logger.info(`init`);
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.send(process.env.APP_NAME || 'app');
+});
+
+// express set up:
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
+app.use(notFound);
+
+if (process.env.NODE_ENV === 'prod') {
+  logger.info(`using production errors handler`);
+  app.use(productionErrors);
+} else {
+  logger.info(`using development errors handler`);
+  app.use(developmentErrors);
+}
 
 // main:
-logger.info(`init`);
+app.set('port', process.env.PORT || 6666);
+const server = app.listen(app.get('port'), () => {
+  logger.info(`app running on port ${server.address().port}`);
+});
